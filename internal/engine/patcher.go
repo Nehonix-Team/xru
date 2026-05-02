a1e2a8825d36636de97ea971901197bdff80e37b
iDevo <idevo.eleazar@gmail.com>
2026-05-02 16:06:52 +0000

    feat: complete XRU engine evolution & VS Code extension update

--- internal/engine/patcher.go
+++ internal/engine/patcher.go
@@ -43,6 +43,14 @@ func ApplyPatch(content string, op PatchOp, path string, patch Value) string {
 		if obj, ok := patch.(Object); ok {
 			return applyRegex(content, obj)
 		}
+	case PatchRPK:
+		if obj, ok := patch.(Object); ok {
+			return renameKeyStructured(content, obj)
+		}
+	case PatchRPV:
+		if obj, ok := patch.(Object); ok {
+			return mergeStructured(content, obj)
+		}
 	}
 	return content
 }
@@ -311,6 +319,28 @@ func applyRegex(content string, patch Object) string {
 				continue
 			}
 			content = re.ReplaceAllString(content, string(val))
+		}
+	}
+	return content
+}
+
+func renameKeyStructured(content string, patch Object) string {
+	for oldK, v := range patch {
+		if newK, ok := v.(Literal); ok {
+			keyPatternQuoted := fmt.Sprintf("%q:", oldK)
+			keyPatternBare := oldK + ":"
+
+			start := strings.Index(content, keyPatternQuoted)
+			if start == -1 {
+				start = strings.Index(content, keyPatternBare)
+			}
+			if start != -1 {
+				if strings.HasPrefix(content[start:], keyPatternQuoted) {
+					content = content[:start] + fmt.Sprintf("%q:", string(newK)) + content[start+len(keyPatternQuoted):]
+				} else {
+					content = content[:start] + string(newK) + ":" + content[start+len(keyPatternBare):]
+				}
+			}
 		}
 	}
 	return content

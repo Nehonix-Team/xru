944f0bf84b9aada25e0fa8edc935f08854cc31f0
iDevo <idevo.eleazar@gmail.com>
2026-05-03 13:30:24 +0000

    style: fix bareword coloration by defining it in the grammar repository

--- apps/main.ts
+++
@@ -1,9 +0,0 @@
-  import { init } from 'core';
-
-    import { Logger } from 'logger';
-    import { Database } from 'db';
-  
-  init();
-
-    console.log("Entire line replaced via Regex!");
-  console.log("Starting...");
